2a27e8d02f71a9cc5262493397f45fccf86ddd4b
InhyeJeong <inhyejeong59@gmail.com>
2019-01-05 17:44:29 +0900

    mocha test using 'Assert module'

--- codlab-nodejs/api/users/user.spec.js
+++ codlab-nodejs/api/users/user.spec.js
@@ -7,11 +7,16 @@
 
 //  import
 const assert = require('assert');
+
 //  첫번째 파라미터 : test suite의 설명을 서술형 문자열로 입력
 //  두번째 파라미터 : 함수입력, 비동기 로직의 콜백 형식으로 (이 안에 it함수로 실제 테스트 코드 작성) 
 describe('GET /users', () => {  
     //  it()함수를 이요애 실제 test code 작성
     it('should return 200 status code', () => {
-      console.log('test 1');
+      //    console.log('test 1');
+      
+      //    검증 로직 시작
+        // 매개변수 두 값이 같으면 pass, 아니면 error를 던짐
+        assert.equal(true, false);
     });
   });
